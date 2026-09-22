2aee316aa97afcc8640a07ea685481746effd4b2
Richard Harris <rharriszzz@gmail.com>
2026-09-22 17:10:02 -0400

    Add bookshelf favicon for browser tabs

--- shelf.py
+++ shelf.py
@@ -411,7 +411,8 @@ def make_handler(shelf, token, app):
                     return self.send(404, {'error': str(exc)})
             assets = {'/': ('index.html', 'text/html; charset=utf-8'),
                       '/app.js': ('app.js', 'text/javascript; charset=utf-8'),
-                      '/style.css': ('style.css', 'text/css; charset=utf-8')}
+                      '/style.css': ('style.css', 'text/css; charset=utf-8'),
+                      '/favicon.svg': ('favicon.svg', 'image/svg+xml')}
             if path in assets:
                 file, mime = assets[path]
                 return self.send(200, (ROOT / 'web' / file).read_bytes(), mime)
